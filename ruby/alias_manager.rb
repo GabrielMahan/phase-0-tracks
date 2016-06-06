c0bb296c2894797e7a5dc3d0b816de6d30a76410
Gabriel Mahan <mahan.gabe@gmail.com>
2016-06-06 11:27:22 -0400

    more fixes

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,13 +2,35 @@
 #Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the 
 #vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 
+def char_changer(char)
+
+	#if it's a vowel
+	if !("aeiou".index(char)==nil)
+		char = "aeiou"["aeiou".index(char)+1]
+		#p char
+	#if it's a consonant
+	elsif !("bcdfghjklmnpqrstvwxyz".index(char) == nil)
+		char = "bcdfghjklmnpqrstvwxyz"["bcdfghjklmnpqrstvwxyz".index(char)+1]
+	#if it's a differnt symbol
+	else
+		char = " "
+	end
+	
+end
+
+
+
 def name_swapper(name)
 	#p name 
-	p name.length
+	#p name.length
 	index = 0
 	new_name = ""
 	while index < name.length
-		new_name[index] = name[index]
+		old_char = name[index]
+		#p old_char
+		new_char = char_changer(old_char)
+		p new_char
+		new_name[index] = new_char.to_s
 		index += 1
 	end
 	p new_name
@@ -16,11 +38,22 @@ def name_swapper(name)
 
 end
 
+#p char_changer("r")
+#p char_changer("a")
 
+response = ""
+until response == "quit"
+	puts "what's full name you want to swap??"
+	response = gets.chomp
+	if response == "quit"
+		response
+	else
+		name_to_translate = response.split.rotate.join(' ')
+		name_swapper(name_to_translate)
+	end
+end
 
-puts "what's full name you want to swap??"
-name_to_translate = gets.chomp.split.rotate.join(' ')
-name_swapper(name_to_translate)
+###JUNK####
 
 #irst_name = name[0]
 #	last_name = name[1]
