a4a346a3e5a5be26a8dafc7db9c4ce2df35a6416
Gabriel Mahan <mahan.gabe@gmail.com>
2016-06-08 21:34:44 -0400

    completed 6.2

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -13,3 +13,16 @@
 #gnawing on furniture
 #frollicking
 
+class Puppy
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+		toy
+	end
+
+end
+
+#needs the .new to initalize an instance....
+#If we wanted, however, we could, in line 17, have it read
+# def fetch.self(toy) to make it a "class method"
+#as written, it's an "instance method"
+Puppy.new.fetch("rope")
--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,57 @@
 class Puppy
-
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
-
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+		toy
+	end
+	def speak(woofer)
+		woofer.to_i.times {puts "woof"}
+	end
+	def roll_over
+		puts "**rolls over**"
+	end
+	def age_converter(years)
+		dog_age = years*7
+		#puts dog_age
+		dog_age
+	end
+	def bark_crasher
+		puts "#{1000.times {puts "bark"}} "
+	end
+	#well that's cool--runs every time before the method
+	def initialize
+		puts "Initializing new puppy instance ..."
+	end
 end
+
+class Dog_trainer
+	def snuggle
+		"puppy snuggle"
+	end
+	def nudge
+		"nudge"
+	end
+	def puppy_chomp
+		"puppy chomp"
+	end
+	def initialize
+		puts "preparing to puppy snuggle"
+	end
+end
+
+#needs the .new to initalize an instance....
+#If we wanted, however, we could, in line 17, have it read
+#def fetch.self(toy) to make it a "class method"
+#as written, it's an "instance method"
+
+#Puppy.new.fetch('rope')
+#Puppy.new.speak(4)
+#Puppy.new.roll_over
+#Puppy.new.age_converter(15)
+#Puppy.new.bark_crasher
+
+trainer_log = []
+50.times {trainer_log << Dog_trainer.new.snuggle}
+trainer_log.each {|value| value << (" #{Dog_trainer.new.nudge}" + " #{Dog_trainer.new.puppy_chomp}") }
+p trainer_log
+
 
