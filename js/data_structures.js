0d966c15994692cf1c1d97a80562d400a984bbf1
Gabriel Mahan <mahan.gabe@gmail.com>
2016-06-16 09:49:07 -0400

    initial work on week 7

---
+++ js/data_structures.js
@@ -0,0 +1,5 @@
+var color_array = ["blue", "blueish", "indigo", "teal"];
+var horse_name_array = ["johnson", "charles", "roberto", "hunchy"];
+horse_name_array.push('big boy');
+color_array.push('silver')
+console.log(color_array + horse_name_array)
