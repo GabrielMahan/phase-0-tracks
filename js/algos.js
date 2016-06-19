8fec0c5da20216210bd985ceef1cb2dc24e8319b
Gabriel Mahan <mahan.gabe@gmail.com>
2016-06-19 18:31:37 -0400

    completed release 2

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,5 @@
-//longest phrase
+
+// Release 0-- longest Phrase // (test code at bottom)
 
 //take the array
 //for each item in it...
@@ -6,10 +7,10 @@
 //add to a blank array if the count is == array length. 
 
 function longest_phrase(array) {
-	blank_array = [];
-	for (i=0; i<array.length; i++)  {
+	var blank_array = [];
+	for (var i=0; i<array.length; i++)  {
 		var array_counter = 0;
-		for (j=0; j<array.length; j++) {
+		for (var j=0; j<array.length; j++) {
 			if (array[i].length >= array[j].length) {
 				array_counter ++;
 			}
@@ -18,28 +19,13 @@ function longest_phrase(array) {
 			blank_array.push(array[i]);
 		} 
 	}
-	console.log(blank_array);
+	//console.log(blank_array);
 	return blank_array; 
 }
-
-//driver / test code
-
-var sample_array_1 = ["long phrase","longest phrase","longer phrase"];
-var sample_array_2 = ["a","abcd","sdfsdfsdfsdfsdf", "asdasfdhkljgad", "fgdkflhgslkjfhgsdfgsfd"];
-var sample_array_3 = ["aaa", "bbb", "dd"];
-longest_phrase(sample_array_1);
-longest_phrase(sample_array_2);
-longest_phrase(sample_array_3);
-
-
-///Release 1 // 
-//Takes two obejcts, 
-//checks to see if objects share a key-value pair (eg)
-
-//take each key in the hash
-////for each value in hash1
-////////for each value in hash 2, see if V,hash1 == V, hash2
-//////////////if it is, then 
+//Release 1 // 
+//for each key in Object 1 (loop)
+//	for each key in Object2, see if V,hash1 == V,hash2 and if Key1 == Key2
+//if it is, then set truth value as true
 
 function pair_checker(object1, object2) {
 	var return_truth_value = false;
@@ -50,34 +36,71 @@ function pair_checker(object1, object2) {
 					return_truth_value = true;
 				}
 			}
-		}
-		
+		}	
 	}
-	console.log(return_truth_value);
+	//console.log(return_truth_value);
 	return return_truth_value;
+}
+/////Release 2//////// (test code at bottom)
+//generate a random char 
+function random_char_generator() {
+	var letters = "abcdefghijklmnopqrstuvqxyz";
+	var random_letter_index = Math.floor(Math.random()*25+1);
+	return letters[random_letter_index];
+}
+//make a word, 1-10 chars long, out of random chars
+function random_word_generator() {
+	var new_string = "";
+	var random_length = Math.floor(Math.random()*10+1);
+	for (var i=0; i<random_length; i++) {
+		new_string += random_char_generator();
+	}
+	return new_string;
+}
+//make an array out of random words
+function random_string_array_generator(number_of_strings) {
+	var random_array = [];
+	for (var i=0; i<number_of_strings; i++) {
+		random_array.push(random_word_generator());
+	}
+	return random_array;
+}
+
+///DRIVER CODE///
+for (var i=0; i<10; i++) {
+	var test_array = [];
+	test_array = random_string_array_generator(4);
+	console.log(test_array);
+	console.log(longest_phrase(test_array));
 }
 
 
-var test_object1 = {name: "steven", age: 54};
-var test_object2 = {name: "Tamir", age: 54};
-var test_object3 = {name: "Tamir", age: 55};
-var test_object4 = {name: "Tamir", fav_color: "blue", age:54};
-var test_object5 = {name: "Tamir", fav_color: "blue", age:52, kilo_weight:54};
+
+//release 0 test code
+
+//var sample_array_1 = ["long phrase","longest phrase","longer phrase"];
+//var sample_array_2 = ["a","abcd","sdfsdfsdfsdfsdf", "asdasfdhkljgad", "fgdkflhgslkjfhgsdfgsfd"];
+//var sample_array_3 = ["aaa", "bbb", "dd"];
+//longest_phrase(sample_array_1);
+//longest_phrase(sample_array_2);
+//longest_phrase(sample_array_3);
+
+
+//Release 1 Test Code  //
+//var test_object1 = {name: "steven", age: 54};
+//var test_object2 = {name: "Tamir", age: 54};
+//var test_object3 = {name: "Tamir", age: 55};
+//var test_object4 = {name: "Tamir", fav_color: "blue", age:54};
+//var test_object5 = {name: "Tamir", fav_color: "blue", age:52, kilo_weight:54};
 
 //should be true
-pair_checker(test_object1, test_object2);
+//pair_checker(test_object1, test_object2);
 
 //should be false
-pair_checker(test_object1, test_object3);
+//pair_checker(test_object1, test_object3);
 
 //should be true
-pair_checker(test_object1, test_object4);
+//pair_checker(test_object1, test_object4);
 
 //should be false!
-pair_checker(test_object1, test_object5);
-
-
-/////Release 2////////
-
-
-
+//pair_checker(test_object1, test_object5);
