73742a39836f52fd5b57bb1fadc8229822114304
Gabriel Mahan <mahan.gabe@gmail.com>
2016-06-19 19:00:21 -0400

    finish 7.2

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,16 @@
 var color_array = ["blue", "blueish", "indigo", "teal"];
 var horse_name_array = ["johnson", "charles", "roberto", "hunchy"];
 horse_name_array.push('big boy');
-color_array.push('silver')
-console.log(color_array + horse_name_array)
+color_array.push('silver');
+console.log(color_array + "  " + horse_name_array);
+
+
+function name_color_pair(names, colors) {
+	var pairs = {};
+	for (var i = 0; i < names.length; i ++ ) {
+		pairs[names[i]] = colors[i];
+	}
+	return pairs;
+}
+
+console.log(name_color_pair(horse_name_array, color_array));
