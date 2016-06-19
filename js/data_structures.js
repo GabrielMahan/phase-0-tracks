f5ae5701ec5a8da402ff5200ffb233031568eb3e
Gabriel Mahan <mahan.gabe@gmail.com>
2016-06-19 19:17:40 -0400

    finish 7.2

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,18 @@ function name_color_pair(names, colors) {
 }
 
 console.log(name_color_pair(horse_name_array, color_array));
+
+
+function cars(make, model, year) {
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.vroom = function() {console.log("vroom vroom")};
+}
+
+var car1 = new cars("Ford", "taurus", 1993);
+console.log(car1);
+car1.vroom();
+
+var car2 = new cars("dodge", "viper", 2003);
+console.log(car2.make);
