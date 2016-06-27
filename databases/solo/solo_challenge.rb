0ac35bc5559b923237fa005bcdd39e921a1cd67e
Gabriel Mahan <mahan.gabe@gmail.com>
2016-06-27 09:56:07 -0400

    add method to print problem

--- databases/solo/solo_challenge.rb
+++ databases/solo/solo_challenge.rb
@@ -12,6 +12,7 @@
 # require gems
 require 'rubygems'
 require 'sqlite3'
+require 'faker'
 
 # Create a database and a tables for students and problems
 quiz = SQLite3::Database.new( "quiz.db" )
@@ -41,10 +42,37 @@ create_problems_table = <<-SQLCOMMANDS
 SQLCOMMANDS
 quiz.execute(create_problems_table)
 
-#add a problem
+#add astudent users
+def add_student_user(student, age, db)
+db.execute("INSERT INTO problems (name, age) VALUES ('#{student}', #{age})")
+end
+#test  add_student_user("ROBERTO", 17, quiz) 
 
-#add a student user
+#400.times do
+#	add_student_user(Faker::Name.name, rand(12..21),quiz)
+#end
+
+
+
+#add a problem
+def add_problem(category,problem_text, response1, response2,response3, response4, correct_answer, db)
+db.execute("INSERT INTO problems (category, problem_text, response1, response2, response3, response4, correct_answer) VALUES ('#{category}', '#{problem_text}', '#{response1}', '#{response2}', '#{response3}', '#{response4}', #{correct_answer})")
+end
+
+categories = ['genetics', 'evolution', 'cells', 'systems']
+
+#400.times do
+#	add_problem(categories[rand(4)], Faker::Lorem.sentence(3),Faker::Lorem.word,Faker::Lorem.word,Faker::Lorem.word,Faker::Lorem.word, rand(1..4),quiz)
+#end
+
+
 
 #print a question
+def print_question(category, problem_number, db)
+problem_array = db.execute("select * from problems WHERE category='#{category}';")
+p problem_array[problem_number]
+end
+print_question(categories[rand(4)],rand(20), quiz)
+
 
 #take an answer, give feedback
