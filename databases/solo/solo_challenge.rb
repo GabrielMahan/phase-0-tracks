2fa0a4004e670f17a02e4ec664e3645f16f67218
Gabriel Mahan <mahan.gabe@gmail.com>
2016-06-26 21:54:55 -0400

    start inital pseudocode for solo challenge

---
+++ databases/solo/solo_challenge.rb
@@ -0,0 +1,14 @@
+# set up a --very-- basic quiz program (I'm a teacher)
+# program needs to 
+	#Accecpt user data, store in bd
+	#Give the user a problem
+	#Give the user feedback
+	#Store the user's feedback
+# Functionality wishlist ... 
+	# to be developed....
+	# Multiple problems, averages
+	# Different Levels
+
+require 'sqlite3'
+
+SQLite3::Database.new( ".db" )
