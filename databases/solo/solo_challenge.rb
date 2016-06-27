9d71c001f79efcb488dc5bf83a1acedd290b8adb
Gabriel Mahan <mahan.gabe@gmail.com>
2016-06-27 00:49:04 -0400

    add basic method to add student

--- databases/solo/solo_challenge.rb
+++ databases/solo/solo_challenge.rb
@@ -9,6 +9,42 @@
 	# Multiple problems, averages
 	# Different Levels
 
+# require gems
+require 'rubygems'
 require 'sqlite3'
 
-SQLite3::Database.new( ".db" )
+# Create a database and a tables for students and problems
+quiz = SQLite3::Database.new( "quiz.db" )
+
+#Create students table
+create_students_table = <<-SQLCOMMANDS
+	CREATE TABLE IF NOT EXISTS students(
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(255),
+	age INTEGER
+	);
+SQLCOMMANDS
+quiz.execute(create_students_table)
+
+#Create problems table
+create_problems_table = <<-SQLCOMMANDS
+	CREATE TABLE IF NOT EXISTS problems(
+	id INTEGER PRIMARY KEY,
+	category VARCHAR(255),
+	problem_text VARCHAR(1000),
+	response1 VARCHAR(255),
+	response2 VARCHAR(255),
+	response3 VARCHAR(255),
+	response4 VARCHAR(255),
+	correct_answer INTEGER
+	);
+SQLCOMMANDS
+quiz.execute(create_problems_table)
+
+#add a problem
+
+#add a student user
+
+#print a question
+
+#take an answer, give feedback
